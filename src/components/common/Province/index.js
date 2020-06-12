6b6921092acfc9611f609f664596c8fc60c1fe19
saeedeyvazy <saeed.eyvazy@gmail.com>
2020-06-12 12:21:28 +0430

    Add Province Call API

---
+++ src/components/common/Province/index.js
@@ -0,0 +1,38 @@
+import { Icon, Picker } from 'native-base'
+import React, { useEffect, useState } from 'react'
+import { getAllProvince } from '../../../redux/actions/province'
+import PickerContainer from '../PickerContainer'
+
+export default Province = ({ onValueChange }) => {
+	const [provinceList, setProvinceList] = useState([])
+	const [selectedProvince, setSelectedProvince] = useState('')
+
+	useEffect(() => {
+		const fetchProvinceList = async () => {
+			const result = await getAllProvince()
+			setProvinceList(result)
+		}
+		fetchProvinceList()
+	}, [])
+
+	return (
+		<PickerContainer>
+			<Picker
+				mode='dropdown'
+				iosIcon={<Icon name='arrow-down' />}
+				style={{ width: undefined }}
+				selectedValue={selectedProvince}
+				onValueChange={(value) => {
+					setSelectedProvince(value)
+					onValueChange(value)
+				}}
+			>
+				<Picker.Item label='Name of province' value='' />
+
+				{provinceList.map((province) => (
+					<Picker.Item label={province} />
+				))}
+			</Picker>
+		</PickerContainer>
+	)
+}
